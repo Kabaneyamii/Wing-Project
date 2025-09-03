351adf376921baa68ed327a15ca9bdf50793ad34
Amwaj <ej13590@gmail.com>
2025-09-03 13:12:39 +0300

    ...

---
+++ database/migrations/2025_09_03_101106_create_notifications_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('notifications', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId('user_id')
+                ->constrained()
+                ->cascadeOnDelete();
+
+            $table->foreignId('from_user_id')
+                ->constrained('users')
+                ->cascadeOnDelete();
+
+            $table->foreignId('post_id')
+                ->nullable()
+                ->constrained()
+                ->cascadeOnDelete();
+
+            $table->string('type');
+
+            $table->boolean('is_read')->default(false);
+
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('notifications');
+    }
+};
